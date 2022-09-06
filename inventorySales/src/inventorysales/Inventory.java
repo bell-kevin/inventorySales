4eaca72eb6de0ca7fe96b77732bb34928ec7d99c
Kevin Bell <bellKevin@pm.me>
2022-09-06 12:56:54 -0600

    Add files via upload

---
+++ inventorySales/src/inventorysales/Inventory.java
@@ -0,0 +1,43 @@
+/** ****************************************************************************
+ * Inventory.java
+ * Kevin Bell
+ *
+ * This super class program simulates inventory sales for food & non-food items.
+ **************************************************************************** */
+package inventorysales;
+
+/**
+ *
+ * @author 4800590195
+ */
+public abstract class Inventory implements Tax {
+
+    int inventoryID, itemQuantityOnHand;
+    String itemName;
+    double unitCost;
+//******************************************************************************
+
+    public Inventory() {
+    } // end zero-parameter constructor
+
+    public Inventory(int inventoryID, int itemQuantityOnHand, String itemName,
+            double unitCost) {
+        this.inventoryID = inventoryID;
+        this.itemQuantityOnHand = itemQuantityOnHand;
+        this.itemName = itemName;
+        this.unitCost = unitCost;
+    } // end constructor
+//******************************************************************************
+
+    public String display() {
+        String result;
+        result = "Inventory #" + inventoryID + ", name" + itemName + ", costs $"
+                + unitCost + ", there are " + itemQuantityOnHand + " on hand";
+        return result;
+    } // end display method
+
+    @Override
+    public double taxCalc(double taxCalc) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+} // end Inventory class
---
+++ inventorySales/src/inventorysales/InventorySales.java
@@ -0,0 +1,14 @@
+/** ****************************************************************************
+ * InventorySales.java
+ * Kevin Bell
+ *
+ * This driver program simulates inventory sales for food & non-food items.
+ **************************************************************************** */
+package inventorysales;
+
+public class InventorySales {
+
+    public static void main(String[] args) {
+        System.out.println("Ch 14 Sales by Kevin Bell \n");
+    } // end main method
+} // end InventorySales class
