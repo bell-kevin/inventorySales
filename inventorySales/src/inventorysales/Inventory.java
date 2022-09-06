24146c193c070f09523fcb7b3071db750c56415f
Kevin Bell <bellKevin@pm.me>
2022-09-06 10:09:35 -0600

    Add files via upload

---
+++ inventorySales/src/inventorysales/Inventory.java
@@ -0,0 +1,15 @@
+/** ****************************************************************************
+ * Inventory.java
+ * Kevin Bell
+ *
+ * This super class program simulates inventory sales for food & non-food items.
+ **************************************************************************** */
+package inventorysales;
+
+public class Inventory {
+
+    int inventoryID;
+    String itemName;
+    int itemQuantityOnHand;
+    double unitCost;
+} // end Inventory class
---
+++ inventorySales/src/inventorysales/InventorySales.java
@@ -0,0 +1,13 @@
+/** ****************************************************************************
+ * InventorySales.java
+ * Kevin Bell
+ *
+ * This driver program simulates inventory sales for food & non-food items.
+ **************************************************************************** */
+package inventorysales;
+
+public class InventorySales {
+
+    public static void main(String[] args) {
+    } // end main method
+} // end InventorySales class
