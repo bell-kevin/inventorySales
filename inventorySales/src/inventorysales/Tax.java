524e483cfae7dd8cb36e681d6844791a84345957
Kevin Bell <bellKevin@pm.me>
2022-09-06 10:39:32 -0600

    Add files via upload

---
+++ inventorySales/src/inventorysales/Tax.java
@@ -0,0 +1,15 @@
+/** ****************************************************************************
+ * Tax.java
+ * Kevin Bell
+ *
+ * This interface simulates tax on inventory sales for food & non-food items
+ **************************************************************************** */
+package inventorysales;
+
+public interface Tax {
+
+    final double FOOD_TAX = .04;
+    final double ITEM_TAX = .10;
+
+    double taxCalc(double taxCalc);
+} // end interface Tax
